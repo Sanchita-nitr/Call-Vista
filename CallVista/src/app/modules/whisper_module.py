5e92eadfe4583382218d019ace1a1c2a98712b02
Sanchita-nitr <sanchitapriyadarshinee@gmail.com>
2025-06-04 00:02:18 +0530

    final commit

--- CallVista/src/app/modules/whisper_module.py
+++
@@ -1,7 +0,0 @@
-import torch
-import whisper
-
-def load_whisper_model():
-    print("Loading Whisper model...")
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    return whisper.load_model("medium", device=device)
